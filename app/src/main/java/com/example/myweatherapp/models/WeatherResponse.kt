cc132f3645e5be576dc5ed793c78fafb6f6d75c7
Beratcan Güreş <guresberatcan@gmail.com>
2021-05-26 22:15:43 +0300

    Created data classes for GSON

---
+++ app/src/main/java/com/example/myweatherapp/models/WeatherResponse.kt
@@ -0,0 +1,22 @@
+package com.example.myweatherapp.models
+
+import com.weatherapp.models.Clouds
+import com.weatherapp.models.Main
+import com.weatherapp.models.Sys
+import com.weatherapp.models.Wind
+import java.io.Serializable
+
+data class WeatherResponse(
+    val coord: Coord,
+    val weather: List<Weather>,
+    val base: String,
+    val main: Main,
+    val visibility: Int,
+    val wind: Wind,
+    val clouds: Clouds,
+    val dt: Int,
+    val sys: Sys,
+    val id: Int,
+    val name: String,
+    val cod: Int,
+) : Serializable
